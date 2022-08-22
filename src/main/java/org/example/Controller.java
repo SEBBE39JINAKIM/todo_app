825925fa3af6353fe78a33a1f7c1d3f62d6cc929
SEBBE39JINAKIM <petitjina1999@gmail.com>
2022-08-22 14:48:42 +0900

    second

---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.web.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String helloWord(){
+        return "To-do Application !";
+    }
+}
